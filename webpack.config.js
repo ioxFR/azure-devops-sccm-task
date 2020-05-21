467046244d189645a3d94ade009147abb2952e8c
Valentin LECERF <valentin.lecerf@vlecerf.com>
2020-05-21 08:21:25 +0200

    Structure updated based on template

--- webpack.config.js
+++ webpack.config.js
@@ -5,12 +5,30 @@ const WebpackCommon = require("./webpack.common.config");
 
 const Target = WebpackCommon.GetTargetPath();
 
+const Settings = {
+    "production": {
+        Tag: "",
+        TaskGuid: "ee4fe03c-58e6-4412-8f78-263466d35a4a",
+    },
+    "development": {
+        Tag: "Dev",
+        TaskGuid: "bb012fdf-81cb-4947-975a-55653f1ace68",
+    }
+    // Can add more flavors here as needed. For example, a flavor for pre-production
+};
+
 module.exports = env => {
+
+    const validEnvs = Object.keys(Settings);
+    if (!validEnvs.includes(env)) {
+        console.error(`BUILD_ENV not set correctly. Allowed values are: ${validEnvs.join(", ")}`);
+        process.exit(1);
+    }
 
     const config = {
 
         entry: {
-            "main": "./buildAndReleaseTask/sccmpowershell.ts",
+            "main": "./src/sccm-task/main.ts",
         },
 
         plugins: [
@@ -26,39 +44,47 @@ module.exports = env => {
                 },
 
                 {
-                    from: path.join(__dirname, "./vss-extension.json"),
-                    to: Target
-                                },
-                {
-                    from: path.join(__dirname, "./buildAndReleaseTask/task.json"),
+                    from: path.join(__dirname, "./src/sccm-task/task.json"),
                     to: path.join(Target, "sccm-task")
                 },
                 {
-                    from: path.join(__dirname, "./images/extension-icon.png"),
+                    from: path.join(__dirname, "./images/icon.png"),
                     to: path.join(Target, "sccm-task", "icon.png")
                 },
                 {
-                    from: path.join(__dirname, "./vss-extension.json"),
+                    from: path.join(__dirname, "./manifests/base.json"),
+                    to: Target
+                },
+                {
+                    from: path.join(__dirname, "./manifests", `${env}.json`),
+                    to: Target
+                },
+                {
+                    from: path.join(__dirname, "./images/icon.png"),
+                    to: Target
+                },
+                {
+                    from: path.join(__dirname, "./src/README.md"),
                     to: Target
                 }
             ]),
 
             WebpackCommon.PackageJsonLoadFixer(Target, [
-                "../buildAndReleaseTask/sccmpowershell.js",
+                "sccm-task/main.js",
             ]),
 
             WebpackCommon.VersionStringReplacer(Target, [
                 "sccm-task/task.json",
-                "vss-extension.json"
+                "base.json"
             ]),
 
             new ReplaceInFileWebpackPlugin([
                 {
                     dir: Target,
                     files: [
-                        "../buildAndReleaseTask/sccmpowershell.js",
+                        "sccm-task/main.js",
                         "sccm-task/task.json",
-                        "vss-extension.json"
+                        "base.json"
                     ],
                     rules: [
                         // This replacement is required to allow azure-pipelines-task-lib to load the 
@@ -66,6 +92,14 @@ module.exports = env => {
                         {
                             search: /__webpack_require__\(.*\)\(resourceFile\)/,
                             replace: 'require(resourceFile)'
+                        },
+                        {
+                            search: /{{taskid}}/ig,
+                            replace: Settings[env].TaskGuid
+                        },
+                        {
+                            search: /{{tag}}/ig,
+                            replace: Settings[env].Tag
                         }
                     ]
                 }
